21ca1182063225144596ea6e565e63f72a83ee15
Lucian Adrian Grijincu <lucian.grijincu@gmail.com>
2009-12-09 05:15:24 +0200

    drv: boot and shutdown LKL
    Signed-off-by: Lucian Adrian Grijincu <lucian.grijincu@gmail.com>

--- src/init.c
+++ src/init.c
@@ -1,14 +1,20 @@
 #include <ddk/ntddk.h>
+#include <asm/env.h>
 
+const int LKL_MEMORY_SIZE = 64 * 1024 * 1024;
+extern int lkl_env_fini(void);
 
 void DDKAPI DriverUnload (PDRIVER_OBJECT driver)
 {
-	DbgPrint("driver unload");
+	DbgPrint("drvpoc: unload compiled at " __TIME__ " on " __DATE__);
+	lkl_env_fini();
+	DbgPrint("drvpoc: unload complete");
 }
 
 NTSTATUS DDKAPI DriverEntry (PDRIVER_OBJECT driver, PUNICODE_STRING registry)
 {
-	DbgPrint("driver entry");
+	DbgPrint("drvpoc: entry compiled at " __TIME__ " on " __DATE__);
 	driver->DriverUnload = DriverUnload;
+	lkl_env_init(LKL_MEMORY_SIZE);
 	return STATUS_SUCCESS;
 }

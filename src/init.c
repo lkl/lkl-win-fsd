9e84eaf80e146509be4afb1b26ba655de1479cd2
Lucian Adrian Grijincu <lucian.grijincu@gmail.com>
2009-12-13 21:43:15 +0200

    doc: add comments to DriverEntry and Driver Unload
    Signed-off-by: Lucian Adrian Grijincu <lucian.grijincu@gmail.com>

--- src/init.c
+++ src/init.c
@@ -11,6 +11,11 @@ static void stop(void)
 	KeWaitForSingleObject(&stopme, Executive, KernelMode, FALSE, NULL);
 }
 
+/*
+ * Called by the system to unload the driver, flush it's data,
+ * terminate all pending operations and free all acquired resources.
+ * @driver - the NTK driver object for this driver
+ */
 void DDKAPI DriverUnload (PDRIVER_OBJECT driver)
 {
 	DbgPrint("drvpoc: unload compiled at " __TIME__ " on " __DATE__);
@@ -18,6 +23,12 @@ void DDKAPI DriverUnload (PDRIVER_OBJECT driver)
 	DbgPrint("drvpoc: unload complete");
 }
 
+/*
+ * Called by the system to initalize the driver
+ * This will initialize LKL and setup the NTK filesystem driver routines.
+ * @driver - the NTK driver object for this driver
+ * @registry - the path to this driver's registry key
+ */
 NTSTATUS DDKAPI DriverEntry (PDRIVER_OBJECT driver, PUNICODE_STRING registry)
 {
 	DbgPrint("drvpoc: entry compiled at " __TIME__ " on " __DATE__);
